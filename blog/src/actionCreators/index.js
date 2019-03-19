fa1f2fb2e8b18bf8ea82b7585d19f4cd40485079
Mike Wong <wongmdesign@gmail.com>
2019-03-19 11:02:49 -0400

    posts added

--- blog/src/actionCreators/index.js
+++ blog/src/actionCreators/index.js
@@ -1,11 +1,13 @@
 import { FETCH_POSTS } from '../actionTypes'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
-import { nextTick } from 'q'
 
+// thunk with action creator
 export const fetchPosts = () => async dispatch => {
   try {
     const response = await jsonPlaceholder.get('/posts')
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: FETCH_POSTS, payload: response.data })
+    // const { data } = await jsonPlaceholder.get('/posts')
+    // dispatch({ type: 'FETCH_POSTS', payload: data })
   } catch (error) {
     console.error(error)
   }
