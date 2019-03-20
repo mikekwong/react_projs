cb0bf6336ef50315e061985f3528c879940baf75
Mike Wong <wongmdesign@gmail.com>
2019-03-20 12:06:47 -0400

    lodashs memoize added for one creator

--- blog/src/actionCreators/index.js
+++ blog/src/actionCreators/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import { FETCH_POSTS, FETCH_USER } from '../actionTypes'
 
 import jsonPlaceholder from '../apis/jsonPlaceholder'
@@ -15,13 +16,41 @@ const fetchPosts = () => async dispatch => {
 }
 
 // fetching the user id (hence id)
-const fetchUser = id => async dispatch => {
+// const fetchUser = id => async dispatch => {
+//   // try {
+//   //   const { data } = await jsonPlaceholder.get(`/users/${id}`)
+//   //   dispatch({ type: FETCH_USER, payload: data })
+//   // } catch (error) {
+//   //   console.error(error)
+//   // }
+// }
+
+// refactor to use lodash's memoize library
+const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch)
+}
+// _ indicates it's a private function
+// we memoize the inner function as everytime it gets called it gets recreated each time its called. so it's easier to memoize that inner function and invoke that reference above
+const _fetchUser = _.memoize(async (id, dispatch) => {
   try {
     const { data } = await jsonPlaceholder.get(`/users/${id}`)
     dispatch({ type: FETCH_USER, payload: data })
   } catch (error) {
     console.error(error)
   }
-}
+})
+
+// fetching the user id (hence id)
+// memoized version of this function and assign it to fetchUser
+// const fetchUser = function (id) {
+//   return _.memoize(async function (dispatch) {
+//     try {
+//       const { data } = await jsonPlaceholder.get(`/users/${id}`)
+//       dispatch({ type: FETCH_USER, payload: data })
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   })
+// }
 
 export { fetchPosts, fetchUser }
